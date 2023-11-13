7509321d4dea93f66049c05dc4e3594ee3f60bb2
Crypto-kiki <ejo2123@naver.com>
2023-11-14 00:17:24 +0900

    prisma

--- index.ts
+++ index.ts
@@ -1,10 +1,18 @@
+// index.ts
 import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
 const port = 3010; // 백엔드 포트 번호
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req: Request, res: Response) => {
-  return res.send("Hello Express!");
+  const body = req.body;
+
+  console.log(body);
+
+  return res.json("Hello Express!");
 });
 
 app.listen(port, () => {
