ca133badbe2c8e94fb58f846ddcd37f58a4183ce
Crypto-kiki <ejo2123@naver.com>
2023-11-15 15:58:00 +0900

    bcrypt

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 // index.ts
 import express, { Express, Request, Response } from "express";
 import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
 
 const app: Express = express();
 const port = 3010;
@@ -13,8 +14,6 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req: Request, res: Response) => {
   const body = req.body;
 
-  console.log(body);
-
   return res.json("Hello Express!");
 });
 
@@ -22,7 +21,6 @@ app.post("/user", async (req, res) => {
   try {
     // 1. 어카운트 패스워드 받아오기
     const { account, password } = req.body;
-    console.log(account, password);
 
     // 2. 유저가 존재하는지 확인
     const existUser = await client.user.findUnique({
@@ -31,8 +29,6 @@ app.post("/user", async (req, res) => {
       },
     });
 
-    console.log(existUser);
-
     // 3. 있으면 리턴 (종료)
     if (existUser) {
       return res
@@ -40,18 +36,31 @@ app.post("/user", async (req, res) => {
         .json({ ok: false, message: "Already exist user." });
     }
 
+    const hashedPassword = bcrypt.hashSync(password, 10);
+    console.log(hashedPassword);
+
+    console.log(bcrypt.compareSync(password, hashedPassword));
+
     // 4. 없으면 생성
     const newUser = await client.user.create({
       data: {
         account,
-        password,
+        password: hashedPassword,
       },
     });
 
     console.log(newUser);
 
     // 5. 생성 후 종료
-    return res.json({ ok: true, user: newUser });
+    return res.json({
+      ok: true,
+      user: {
+        id: newUser.id,
+        createdAt: newUser.createdAt,
+        updatedAt: newUser.updatedAt,
+        account: newUser.account,
+      },
+    });
   } catch (error) {
     console.error(error);
   }
